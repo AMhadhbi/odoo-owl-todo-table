e6ed2362bceb449ca670eb3157db844bd9da0eaa
Achraf Mhadhbi <mhadhbi.achref@gmail.com>
2024-11-17 12:38:02 +0100

    ADD Create dynamic task

--- src/app.js
+++ src/app.js
@@ -7,13 +7,18 @@ class Root extends Component {
   <div class="m-0 p-4 bg-white rounded">
   <!-- Add Task Button -->
   <div>
-    <button class="btn btn-primary mb-3" type="button">Add New Task</button>
+    <button class="btn btn-primary mb-3" type="button" t-on-click="addTask">Add New Task</button>
   </div>  
 
   <!-- Input Fields -->
   <div class="input-group-lg bg-white rounded border d-flex w-100 align-items-center mb-3">
-    <input type="text" class="form-control-lg fs-5 flex-fill border-0" placeholder="Task Name" name="taskName"/>
-    <input type="color" class="form-control-lg border-0 bg-white form-control-color ms-2" value="#563d7c" title="Choose Task Color"/>
+    <input type="text" 
+    class="form-control-lg fs-5 flex-fill border-0" 
+    placeholder="Task Name" name="taskName"
+    t-att-value="state.name" t-model="state.name"/>
+    <input type="color" 
+    class="form-control-lg border-0 bg-white form-control-color ms-2" title="Choose Task Color"
+    t-att-value="state.color" t-model="state.color"/>
   </div>
   
   <!-- Task Table -->
@@ -39,7 +44,7 @@ class Root extends Component {
         </div>
         </td>
         <td>
-          <input type="color" class="form-control-color border-0" value="#563d7c"/>
+          <input type="color" class="form-control-color border-0" t-att-id="task.id" t-att-value="state.color"/>
         </td>
         <td>
           <button class="btn btn-primary me-2"><i class="bi bi-pencil"></i></button>
@@ -54,12 +59,40 @@ class Root extends Component {
   `
 
   setup(){
-    this.tasks = useState([
-      {id:1, name:"Task 1", color:"fff000", isCompleted: false},
-      {id:2, name:"Task 2", color:"fff000", isCompleted: false},
-      {id:3, name:"Task 3", color:"fff000", isCompleted: false},
-    ])
+    this.state = useState({
+      name: "",
+      color: "#FFF700",
+      isCompleted: false,
+      isEditing: false
+  });
+
+    this.tasks = useState([])
   }
+
+  // function to add task
+  addTask(){
+    // do not allow empty task name
+  if (!this.state.name){
+      alert("Please add task.")
+      return
+  }
+
+  // add unique id
+  const id = Math.random().toString().substring(2,12)
+
+  // add new task
+  this.tasks.push({
+      id,
+      name: this.state.name,
+      color: this.state.color,
+      isCompleted: this.state.isCompleted,
+  })
+
+  // reset states after saving
+  let state = this.state
+    this.state = {...state, name:"", color: "#FFF700"}
+  }
+  
 
 }
 
